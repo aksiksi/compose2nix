7fdd01c6bd6f81970ebbeb50068f9b12569ab92d
Assil Ksiksi <cyph0nik@gmail.com>
2024-03-17 08:00:25 -0400

    simplify NixOS test setup

--- nixos-test/test.nix
+++ nixos-test/test.nix
@@ -1,20 +1,21 @@
 { pkgs, ... }:
 
 let
-  # Use pre-pulled images to avoid pulling having to pull images
-  # inside the test VMs.
+  # Use pre-pulled image to avoid having to pull images inside the test VMs.
   # https://nixos.org/manual/nixpkgs/stable/#ssec-pkgs-dockerTools-fetchFromRegistry
-  sabnzbdImage = pkgs.dockerTools.pullImage {
-    imageName = "lscr.io/linuxserver/sabnzbd";
-    finalImageTag = "latest";
-    imageDigest = "sha256:10de547c287d318200b776ccd2adb7e826a0040e70287877b92a9f9ceccd6840";
-    sha256 = "sha256-jeOo0b1SvCK1cQ9lxq3Pt1CWVRtu6RqiFw/tdzWjEkc=";
+  nginxImage = pkgs.dockerTools.pullImage {
+    imageName = "docker.io/library/nginx";
+    finalImageTag = "stable-alpine-slim";
+    imageDigest = "sha256:f5fb3bd2fc68f768b81bccad0161f8100ac52b2de4d7b6128421edd2ce136296";
+    sha256 = "sha256-yRDW3G/JA4WjVOul4zCHE/Xnpk+7qPGtkueiFje6EOE=";
   };
-  radarrImage = pkgs.dockerTools.pullImage {
-    imageName = "lscr.io/linuxserver/radarr";
-    finalImageTag = "develop";
-    imageDigest = "sha256:7e5682d40b8ff3276df8b71cc420bd9c714dbc9e22b2f4110055999c75802452";
-    sha256 = "sha256-GcZZFMjSBOjdMDm9sZ1AGt8pPNIXLwxXRERUjFgNNHs=";
+  common = {
+    # Container DNS.
+    networking.firewall.allowedUDPPorts = [ 53 ];
+    virtualisation.graphics = false;
+    virtualisation.oci-containers.containers."myproject-service-a".imageFile = nginxImage;
+    virtualisation.oci-containers.containers."service-b".imageFile = nginxImage;
+    system.stateVersion = "23.05";
   };
 in
 {
@@ -24,24 +25,12 @@ in
       imports = [
         ./docker-compose.nix
       ];
-      # Container DNS.
-      networking.firewall.allowedUDPPorts = [ 53 ];
-      virtualisation.graphics = false;
-      virtualisation.oci-containers.containers."radarr".imageFile = radarrImage;
-      virtualisation.oci-containers.containers."myproject-sabnzbd".imageFile = sabnzbdImage;
-      system.stateVersion = "23.05";
-    };
+    } // common;
     podman = { pkgs, ... }: {
       imports = [
         ./podman-compose.nix
       ];
-      # Container DNS.
-      networking.firewall.allowedUDPPorts = [ 53 ];
-      virtualisation.graphics = false;
-      virtualisation.oci-containers.containers."radarr".imageFile = radarrImage;
-      virtualisation.oci-containers.containers."myproject-sabnzbd".imageFile = sabnzbdImage;
-      system.stateVersion = "23.05";
-    };
+    } // common;
   };
   testScript = ''
     d = {"docker": docker, "podman": podman}
@@ -52,15 +41,15 @@ in
     for runtime, m in d.items():
       m.succeed("mkdir -p /mnt/media")
       m.succeed("mkdir -p /mnt/media/Books")
-      m.succeed("mkdir -p /var/volumes/radarr")
-      m.succeed("mkdir -p /var/volumes/sabnzbd")
+      m.succeed("mkdir -p /var/volumes/service-a")
+      m.succeed("mkdir -p /var/volumes/service-b")
 
     for runtime, m in d.items():
       # Wait for root Compose service to come up.
       m.wait_for_unit(f"{runtime}-compose-myproject-root.target")
 
       # Wait for container services.
-      m.wait_for_unit(f"{runtime}-radarr.service")
-      m.wait_for_unit(f"{runtime}-myproject-sabnzbd.service")
+      m.wait_for_unit(f"{runtime}-myproject-service-a.service")
+      m.wait_for_unit(f"{runtime}-service-b.service")
   '';
 }
