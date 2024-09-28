26cd2f5d67e877108f95a30ef38dd10a5a020e5f
Assil Ksiksi <assil@ksiksi.net>
2024-09-28 12:34:57 -0400

    fix: enable container DNS for Podman in Nix header
    This is required because we don't use the default Podman network.
    
    See: https://github.com/NixOS/nixpkgs/issues/226365

--- testdata/TestNoRestart.podman.nix
+++ testdata/TestNoRestart.podman.nix
@@ -11,6 +11,11 @@
       dns_enabled = true;
     };
   };
+
+  # Enable container name DNS for non-default Podman networks.
+  # https://github.com/NixOS/nixpkgs/issues/226365
+  networking.firewall.interfaces."podman+".allowedUDPPorts = [ 53 ];
+
   virtualisation.oci-containers.backend = "podman";
 
   # Containers
